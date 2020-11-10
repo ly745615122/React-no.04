80c4de05951c542cd99418580eed6e6aafe7014b
ly745615122 <745615122@qq.com>
2020-11-10 16:45:23 +0800

    --

---
+++ precache-manifest.4664114e1d52f490e5e3e022ae5aacad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b326f106d5332e37a1f18253a11d90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "824fd5ca8cd1bd3b77e7",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "0def605bda9a13a2a6a0",
+    "url": "/static/css/main.0ed9304c.chunk.css"
+  },
+  {
+    "revision": "824fd5ca8cd1bd3b77e7",
+    "url": "/static/js/2.57cb8ed7.chunk.js"
+  },
+  {
+    "revision": "3530bbda1c47bd33b8cccf03b351d584",
+    "url": "/static/js/2.57cb8ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0def605bda9a13a2a6a0",
+    "url": "/static/js/main.48ee90c3.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
